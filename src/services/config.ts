44bef87e9b0db93912ce59e0e6edeb63c6869b2f
lakshannaveen <139128804+lakshannaveen@users.noreply.github.com>
2025-12-16 15:33:01 +0530

    Update config.ts

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,4 +1,4 @@
 // Shared API configuration
-//export const API_BASE_URL = import.meta.env.VITE_API_URL || "https://cartrackerbackend.dockyardsoftware.com/api"
-export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:60748/api"
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "https://cartrackerbackend.dockyardsoftware.com/api"
+//export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:60748/api"
 
